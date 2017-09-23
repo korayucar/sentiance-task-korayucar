a36313167582452689dc85056e072757e6bd6d62
Koray Ucar <kryucar@gmail.com>
2017-09-23 16:48:52 +0300

    improve design document

---
+++ random-data-generator/src/main/java/xyz/korayucar/sentiance/random/MasterDataSet.java
@@ -0,0 +1,11 @@
+package xyz.korayucar.sentiance.random;
+
+import java.util.Map;
+
+/**
+ * Created by koray on 23/09/17.
+ */
+public class MasterDataSet {
+
+    Map<String,Long> totalSizeOfModules;
+}
